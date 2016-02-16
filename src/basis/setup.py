49e90dac09fddc311de2faed1d31d544847d217e
Michael Schuurman <mschuurm@gmail.com>
2016-02-15 20:46:29 -0500

    update cython setup

---
+++ src/basis/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+ext_modules=[
+    Extension("gaussian",sources=["gaussian.pyx"],libraries=["m"])
+]
+
+setup(
+  name = "Gaussian",
+  ext_modules = cythonize(ext_modules)
+)
