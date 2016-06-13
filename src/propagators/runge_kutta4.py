90b6a5dcaa50e0f748b0d6757e9dae6d7d1084c0
Michael Schuurman <schuurm@anyuta.sims.nrc.ca>
2016-06-13 13:14:18 -0400

    updated RK4 propagator. Readily generalizable to RKn

--- src/propagators/runge_kutta4.py
+++ src/propagators/runge_kutta4.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 import numpy as np
 import src.fmsio.glbl as glbl
 import src.dynamics.timings as timings
@@ -18,71 +19,72 @@ import src.dynamics.surface as surface
 def propagate_bundle(master, dt):
 
     timings.start('propagators.propagate_bundle')
-    dt_2 = dt / 2.
-    dt_6 = dt / 6.
     ncrd = glbl.fms['num_particles'] * glbl.fms['dim_particles']
     mass = master.traj[0].masses()
 
-    amp0 = master.amplitudes()
     x0   = np.zeros((master.nalive,ncrd),dtype=np.float)
-
+    p0   = np.zeros((master.nalive,ncrd),dtype=np.float)
+    g0   = np.zeros(master.nalive,dtype=np.float)
     xnew = np.zeros((master.nalive,ncrd),dtype=np.float)
     pnew = np.zeros((master.nalive,ncrd),dtype=np.float)
     gnew = np.zeros(master.nalive,dtype=np.float)
 
     # initialize position,momentum,phase
+    amp0 = master.amplitudes()
     for i in range(master.nalive):
         ii = master.alive[i]
         x0[i,:]   = master.traj[ii].x()
+        p0[i,:]   = master.traj[ii].p()
+        g0[i]     = master.traj[ii].gamma()
         xnew[i,:] = master.traj[ii].x()
         pnew[i,:] = master.traj[ii].p()
-        gnew[i]   = master.traj[ii].phase
+        gnew[i]   = master.traj[ii].gamma()
 
     # 
     # determine k1, k2, k3, k4
     #
-    t_step = [dt_2, dt_2, dt]
-    k_mult = [1., 2., 2., 1.]
-    r_ordr = 4
+    rk_ordr = 4
+    k_mult  = [1, 2, 2, 1]
+    t_step  = [dt / k_mult[i] for i in range(1,len(k_mult))]
+    dt_seg = dt / sum(k_mult)
 
     #
     # Do 4th order RK
     #    
     H_list = []
-    a_list = [amp0]
-    for rk in range(r_ordr):
-    
+    for rk in range(rk_ordr):
+
         # determine x,p,gamma at f(t,x)
         H_list.append(master.Heff)
         for i in range(master.nalive):
             ii = master.alive[i]
 
-            vk = master.traj[ii].velocity()
-            fk = master.traj[ii].force()
-            gk = master.traj[ii].phase_dot()        
+            xdot = master.traj[ii].velocity()
+            pdot = master.traj[ii].force()/mass
+            gdot = master.traj[ii].phase_dot()
 
-            xnew[i,:] += dt_6 * k_mult[rk] * vk
-            pnew[i,:] += dt_6 * k_mult[rk] * fk/mass
-            gnew[i]   += dt_6 * k_mult[rk] * gk
-            if rk != (r_ordr-1):
-                master.traj[ii].update_x(x0[i,:] + t_step[rk]*vk)
+            xnew[i,:] += dt_seg * k_mult[rk] * xdot
+            pnew[i,:] += dt_seg * k_mult[rk] * pdot
+            gnew[i]   += dt_seg * k_mult[rk] * gdot
+
+            if rk != (rk_ordr-1):
+                master.traj[ii].update_x( x0[i,:]   + t_step[rk] * xdot)
+                master.traj[ii].update_p( p0[i,:]   + t_step[rk] * pdot)
+                master.traj[ii].update_phase( g0[i] + t_step[rk] * gdot)
             else:
-                master.traj[ii].update_x(xnew[i,:])
-                master.traj[ii].update_p(pnew[i,:])
-                master.traj[ii].update_phase(gnew[i])
-        
+                master.traj[ii].update_x( xnew[i,:] )
+                master.traj[ii].update_p( pnew[i,:] )
+                master.traj[ii].update_phase( gnew[i] )
+       
         # update potential energy surfaces and Heff        
         surface.update_pes(master)
-        if rk != (r_ordr-1):
-            master.update_amplitudes(t_step[rk],10)
-            a_list.append(master.amplitudes())
-            master.set_amplitudes(amp0)
-        else:
-            ampn    = amp0
-            for i in range(r_ordr):
-                ampn += dt_6 * k_mult[i] * -np.complex(0.,1.) * np.dot(H_list[i], a_list[i])
-            master.set_amplitudes(ampn)
 
+    amp_sum = 0
+    for i in range(len(k_mult)):
+        master.update_amplitudes(dt,10,H_list[i],amp0)
+        amp_sum += k_mult[i] * master.amplitudes()
+    master.set_amplitudes(amp_sum/sum(k_mult))
+   
     #
     timings.stop('propagators.propagate_bundle')
     return
@@ -90,20 +92,20 @@ def propagate_bundle(master, dt):
 #
 # propagate a single trajectory
 #
-def propagate_traj(traj, dt):
+def propagate_trajectory(traj, dt):
     
     timings.start('propagators.propagate_trajectory')
 
-    dt_2 = dt / 2.
-    dt_6 = dt / 6.
     ncrd = glbl.fms['num_particles'] * glbl.fms['dim_particles']
+    mass = master.traj[0].masses()
 
     # 
     # determine k1, k2, k3, k4
     #
-    t_step = [dt_2, dt_2, dt]
-    k_mult = [1.,2.,2.,1.]
-    r_ordr = 4
+    rk_ordr = 4
+    k_mult  = [1, 2, 2, 1]
+    t_step  = [dt / k_mult[i] for i in range(1,len(k_mult))]
+    dt_seg  = dt / sum(k_mult)
 
     # 
     # initialize values
@@ -111,26 +113,26 @@ def propagate_traj(traj, dt):
     x0   = traj.x()
     xnew = traj.x()
     pnew = traj.p()
-    gnew = traj.phase
+    gnew = traj.gamma()
 
     #
     # Do 4th order RK
     #    
-    for rk in range(r_ordr):
+    for rk in range(rk_ordr):
 
-        vk = traj.velocity()
-        fk = traj.force()
-        gk = traj.phase_dot()
+        xdot = traj.velocity()
+        pdot = traj.force()/mass
+        gdot = traj.phase_dot()
 
-        xnew += dt_6 * k_mult[rk] * vk
-        pnew += dt_6 * k_mult[rk] * fk/mass
-        gnew += dt_6 * k_mult[rk] * gk
-        if rk != (r_ordr-1):
-            traj.update_x(x0 + t_step[rk]*vk)
+        xnew += dt_seg * k_mult[rk] * xdot 
+        pnew += dt_seg * k_mult[rk] * pdot
+        gnew += dt_seg * k_mult[rk] * gdot
+        if rk != (rk_ordr-1):
+            traj.update_x( x0 + t_step[rk]*xdot )
         else:
-            traj.update_x(xnew)
-            traj.update_p(pnew)
-            traj.update_phase(gnew)
+            traj.update_x( xnew )
+            traj.update_p( pnew )
+            traj.update_phase( gnew )
 
     timings.stop('propagators.propagate_trajectory')
     return
