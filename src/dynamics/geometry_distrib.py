a7f3127b6dc1a457e0de55e4867d11717831a932
Michael Schuurman <mschuurm@gmail.com>
2016-06-17 23:49:16 -0400

    update read_geometry

--- src/dynamics/geometry_distrib.py
+++ src/dynamics/geometry_distrib.py
@@ -1,16 +1,31 @@
+import src.fmsio.glbl as glbl
+import src.dynamics.utilities as utils
+import src.basis.trajectory as trajectory
+
 #
 #  Take initial position and momentum from geometry.dat file
 #
 def sample_distribution(master):
-    geom = load_geometry()
-    amp  = complex(1.,0.)
-    # add a single trajectory specified by geometry.dat
-    master.add_trajectory(trajectory.trajectory(
+
+    amps,geoms = utils.load_geometry()
+    ngeoms = len(amps)
+    natms  = int(len(geoms)/ngeoms)
+
+    for i in range(ngeoms):
+        amp  = amps[i]
+        geom = []
+   
+        # load particles in geometry array
+        for j in range(natms):
+            geom.append(geoms[i*natms + j])
+
+        # add a single trajectory specified by geometry.dat
+        master.add_trajectory(trajectory.trajectory(
                           glbl.fms['n_states'],
                           particles=geom,
                           parent=0))
-    # ...with unit amplitude
-    master.traj[i].amplitude = amp
+        # ...with unit amplitude
+        master.traj[i].amplitude = amp
 
     # state of trajectory not set, return false
     return False
