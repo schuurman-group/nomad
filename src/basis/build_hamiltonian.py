Bug fixes in: (1) the calculation of the non-adiabatic coupling terms derived from a vibronic coupling Hamiltonian, and;               (2) build_hamiltonian - the on-diagonal elements are now correctly calculated when using centroids.

src/basis/build_hamiltonian.py
@@ -70,6 +70,7 @@ def build_hamiltonian(intlib,traj_list,traj_alive,cent_list=None):
     Heff     = np.zeros((n_alive,n_alive),dtype=np.complex)
 
     for ij in range(n_elem):
+    
         i,j = ij_ind(ij)
         ii  = traj_alive[i]
         jj  = traj_alive[j]
@@ -97,8 +98,10 @@ def build_hamiltonian(intlib,traj_list,traj_alive,cent_list=None):
 
         # potential energy matrix
         if req_centroids:
-            V[i,j]  =  v_int(traj_list[ii], traj_list[jj],
-                            cent_list[c_ind(ii,jj)],S_ij=S[i,j])
+            if i == j:
+                V[i,j]  =  v_int(traj_list[ii], traj_list[jj],traj_list[ii],S_ij=S[i,j])
+            else:
+                V[i,j]  =  v_int(traj_list[ii], traj_list[jj],cent_list[c_ind(ii,jj)],S_ij=S[i,j])
         else:
             V[i,j] =  v_int(traj_list[ii], traj_list[jj], S_ij=S[i,j])
         V[j,i]     = V[i,j].conjugate()

src/spawn/forward_backward.py
@@ -69,12 +69,12 @@ def spawn(master,dt):
                 continue
 
             # compute magnitude of coupling to state j
-            #coup = master.traj[i].coup_dot_vel(st)
             coup = master.traj[i].eff_coup(st)
+
             coup_hist[i][st,:] = np.roll(coup_hist[i][st,:],1)
             coup_hist[i][st,0] = coup
 
-            # if we satisfy spawning conditions, begin spawn process                    
+            # if we satisfy spawning conditions, begin spawn process
             if spawn_trajectory(master.traj[i], st, coup_hist[i][st,:], current_time):
                 parent       = trajectory.copy_traj(master.traj[i])
                 child        = trajectory.copy_traj(parent)
@@ -205,11 +205,12 @@ def spawn_trajectory(traj, spawn_state, coup_hist, current_time):
 
     # there is insufficient coupling
     #if traj.coup_dot_vel(spawn_state) < glbl.fms['spawn_coup_thresh']:
-    if traj.eff_coup(spawn_state) < glbl.fms['spawn_coup_thresh']:
+    if abs(traj.eff_coup(spawn_state)) < glbl.fms['spawn_coup_thresh']:
         return False
 
     # if coupling is decreasing
-    if coup_hist[0] < coup_hist[1]:
+    #if coup_hist[0] < coup_hist[1]:
+    if abs(coup_hist[0]) < abs(coup_hist[1]):
         return False
 
     return True