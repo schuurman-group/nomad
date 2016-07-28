c7ba4c13061eceb93d18ddee47c4778262780734
Michael Coates <mrc13183@gmail.com>
2016-07-28 14:42:13 -0400

    changed collocation back to what it was before

--- src/integrals/collocation.py
+++ src/integrals/collocation.py
@@ -27,14 +27,14 @@ def v_integral(traj1, traj2=None, centroid=None, S_ij=None):
     # state
     elif traj1.state == traj2.state:
         # Adiabatic energy
-        return traj1.energy(traj1.state) * traj1.h_overlap(traj2)
+        return traj1.energy(traj1.state) * traj2.h_overlap(traj1)
     #
     # off-diagonal matrix elements between trajectories on different 
     # elecronic states
     elif traj1.state != traj2.state:
         # Derivative coupling
-        fij = traj1.derivative(traj2.state)
-        v = np.dot(fij, traj1.deldx_m(traj2))
+        fij = traj2.derivative(traj1.state)
+        v = np.dot(fij, traj2.deldx_m(traj1))
         return v
     
     else:
@@ -50,13 +50,13 @@ def ke_integral(traj1, traj2, S_ij=None):
     if traj1.state == traj2.state:
         if glbl.fms['interface'] == 'vibronic':
             for i in range(traj1.n_particle):
-                ke -= (traj1.particles[i].deld2x(traj2.particles[i])
+                ke -= (traj2.particles[i].deld2x(traj1.particles[i])
                        * ham.freq[i]/2.0)
         else:
             for i in range(traj1.n_particle):
-                ke -= (traj1.particles[i].deld2x(traj2.particles[i]) /
-                       (2.*traj1.particles[i].mass))
-        return ke * traj1.h_overlap(traj2)
+                ke -= (traj2.particles[i].deld2x(traj1.particles[i]) /
+                       (2.*traj2.particles[i].mass))
+        return ke * traj2.h_overlap(traj1)
     else:
         return ke
     
@@ -66,6 +66,6 @@ def ke_integral(traj1, traj2, S_ij=None):
 def sdot_integral(traj1, traj2, S_ij=None):
     sdot =  np.dot( traj2.velocity(), traj1.deldx(traj2) ) + \
             np.dot( traj2.force()   , traj1.deldp(traj2) ) + \
-            complex(0.,1.) * traj2.phase_dot() * traj1.h_overlap(traj2)
+            complex(0.,1.) * traj2.phase_dot() * traj2.h_overlap(traj1)
     return sdot
     
