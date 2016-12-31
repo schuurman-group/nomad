fixed centroid issue: (i,j) -> (j,i) [change sign of derivative coupling when necessary]

src/basis/bundle.py
@@ -44,7 +44,7 @@ def copy_bundle(orig_bundle):
             new_bundle.cent.append([None for j in range(orig_bundle.n_traj())])
             for j in range(i):
                 new_bundle.cent[i][j] = centroid.copy_cent(orig_bundle.cent[i][j])
-                new_bundle.cent[j][i] = new_bundle.cent[i][j]
+                new_bundle.cent[j][i] = centroid.copy_cent(orig_bundle.cent[j][i])
 
     return new_bundle
 
@@ -285,77 +285,47 @@ class Bundle:
 
         Currently only includes energy from alive trajectories
         """
-        nalive = len(self.alive)
-        v_vec = np.array([self.traj[self.alive[i]].potential()
-                          for i in range(nalive)])
-        return sum(v_vec).real / nalive
-#        weight = np.array([self.traj[self.alive[i]].amplitude *
-#                           self.traj[self.alive[i]].amplitude.conjugate() 
-#                           for i in range(len(self.alive))])
-#        v_int  = np.array([self.V[i,i] 
-#                           for i in range(self.nalive)])
-#        return np.dot(weight, v_int).real
+        nalive  = len(self.alive)
+        wgts    = np.array([np.conj(self.traj[self.alive[i]].amplitude)*
+                                   self.traj[self.alive[i]].amplitude  
+                                   for i in range(nalive)])
+        nrm     = np.sqrt(np.dot(wgts,wgts))
+        pot_vec = np.array([self.traj[self.alive[i]].potential()
+                                    for i in range(nalive)])
+        return (np.dot(wgts,pot_vec) / nrm).real
 
     @timings.timed
     def pot_quantum(self):
         """Returns the QM (coupled) potential energy of the bundle.
         Currently includes <live|live> (not <dead|dead>,etc,) contributions...
         """
+#        return np.dot(np.dot(
+#               np.conj(self.amplitudes()),self.V),self.amplitudes()).real
+        Sinv = np.linalg.pinv(self.S)
         return np.dot(np.dot(
-               np.conj(self.amplitudes()),self.V),self.amplitudes()).real
-#        energy = 0.
-#        for i in range(len(self.alive)):
-#            ii = self.alive[i]
-#            weight = (self.traj[ii].amplitude *
-#                      self.traj[ii].amplitude.conjugate())
-#            v_int  = self.V[i,i]
-#            energy += (weight * v_int).real
-#            for j in range(len(self.alive)):
-#                jj     = self.alive[j]
-#                weight = (self.traj[jj].amplitude *
-#                          self.traj[ii].amplitude.conjugate())
-#                v_int  = self.V[i,j]
-#                print("pot quan="+str(weight * v_int))
-#                energy += (weight * v_int).real
-#        return energy
+               np.conj(self.amplitudes()),np.dot(Sinv,self.V)),self.amplitudes()).real
 
     @timings.timed
     def kin_classical(self):
         """Returns the classical kinetic energy of the bundle."""
         nalive = len(self.alive)
-        ke_vec = np.array([np.dot(self.traj[self.alive[i]].p()*
-                                  self.traj[self.alive[i]].p(),
-                                  self.traj[self.alive[i]].interface.kecoeff)
+        wgts   = np.array([np.conj(self.traj[self.alive[i]].amplitude)*
+                                   self.traj[self.alive[i]].amplitude  
+                                   for i in range(nalive)])
+        nrm    = np.sqrt(np.dot(wgts,wgts))
+        ke_vec = np.array([np.dot(self.traj[self.alive[i]].p()**2, 
+                                  self.traj[self.alive[i]].interface.kecoeff) 
                                   for i in range(nalive)])
-        return sum(ke_vec).real / nalive
-#        weight = np.array([self.traj[self.alive[i]].amplitude *
-#                           self.traj[self.alive[i]].amplitude.conjugate() 
-#                           for i in range(len(self.alive))])
-#        ke_int  = np.array([self.T[i,i] 
-#                           for i in range(self.nalive)])
-#        return np.dot(weight, ke_int).real
+        return (np.dot(wgts,ke_vec).real / nrm).real
 
     @timings.timed
     def kin_quantum(self):
         """Returns the QM (coupled) kinetic energy of the bundle."""
-        Sinv = np.linalg.pinv(self.traj_ovrlp)
+#         return np.dot(np.dot(
+#               np.conj(self.amplitudes()),self.T),self.amplitudes()).real
+        Sinv = np.linalg.pinv(self.S)
         return np.dot(np.dot(
-               np.conj(self.amplitudes()),self.T),self.amplitudes()).real
-#        energy = 0.
-#        for i in range(len(self.alive)):
-#            ii = self.alive[i]
-#            weight = (self.traj[ii].amplitude *
-#                      self.traj[ii].amplitude.conjugate())
-#            ke_int = self.T[i,i]
-#            energy += (weight * ke_int).real
-#            for j in range(len(self.alive)):
-#                jj     = self.alive[j]
-#                weight = (self.traj[jj].amplitude *
-#                          self.traj[ii].amplitude.conjugate())
-#                ke_int = self.T[i,j]
-#                print("kin quan="+str(weight * ke_int))
-#                energy += (weight * ke_int).real
-#        return energy
+               np.conj(self.amplitudes()),np.dot(Sinv,self.T)),self.amplitudes()).real
 
     def tot_classical(self):
         """Returns the total classical energy of the bundle."""
@@ -427,7 +397,7 @@ class Bundle:
                 if self.cent[i][j] is None:
                     self.cent[i][j] = centroid.Centroid(traj_i=self.traj[i],
                                                         traj_j=self.traj[j])
-                    self.cent[j][i] = self.cent[i][j]
+                    self.cent[j][i] = self.cent[i][j].hermitian()
 
     @timings.timed
     def update_matrices(self):
@@ -566,8 +536,8 @@ class Bundle:
                 fileio.print_bund_mat(self.time, 't_ovrlp.dat', self.traj_ovrlp) 
             fileio.print_bund_mat(self.time, 's.dat', self.S)
             fileio.print_bund_mat(self.time, 'h.dat', self.T+self.V)
- #           fileio.print_bund_mat(self.time, 't.dat', self.T)
- #           fileio.print_bund_mat(self.time, 'v.dat', self.V)
+            fileio.print_bund_mat(self.time, 't.dat', self.T)
+            fileio.print_bund_mat(self.time, 'v.dat', self.V)
             fileio.print_bund_mat(self.time, 'heff.dat', self.Heff)
             fileio.print_bund_mat(self.time, 'sdot.dat', self.Sdot)
 

src/basis/centroid.py
@@ -27,7 +27,7 @@ def copy_cent(orig_cent):
     new_cent.pes_geom   = copy.deepcopy(orig_cent.pes_geom)
     new_cent.poten      = copy.deepcopy(orig_cent.poten)
     new_cent.deriv      = copy.deepcopy(orig_cent.deriv)
-    new_cent.pes_data   = orig_cent.interface.copy_data(orig_cent.pes_data)
+    new_cent.pes_data   = orig_cent.interface.copy_surface(orig_cent.pes_data)
     return new_cent
 
 class Centroid:
@@ -97,7 +97,31 @@ class Centroid:
 
     #----------------------------------------------------------------------
     #
-    # Functions for setting basic pes information from trajectory
+    # Return a new centroid object corresponding to the (j,i) complement to the
+    #  current (i,j) object
+    #  -- Note: this is likely more appropriate as an 'interface' routine,
+    #           as centroid isn't likely to know this info
+    #----------------------------------------------------------------------
+    def hermitian(self):
+        """Return a new centroid object corresponding to the (j,i) complement
+           of the current (i,j) object"""
+        new_cent = copy_cent(self)
+
+        # if we have no data, just return new centroid object
+        if new_cent.pes_data is None:
+            return new_cent
+
+        # change sign of derivative coupling
+        if new_cent.pstates[0] != new_cent.pstates[1]:
+            if 'deriv' in new_cent.pes_data.data_keys:
+                new_cent.pes_data.grads[new_cent.pstates[1]] *= -1.
+                new_cent.deriv *= -1.
+
+        return new_cent
+
+    #----------------------------------------------------------------------
+    #
+    # Functions for setting basic pes information from centroid 
     #
     #----------------------------------------------------------------------
     def update_x(self, pos):
@@ -112,11 +136,11 @@ class Centroid:
 
     def update_pes(self, pes_info):
         """Updates information about the potential energy surface."""
-        self.pes_data   = self.interface.copy_data(pes_info)
+        self.pes_data   = self.interface.copy_surface(pes_info)
         self.pes_geom   = self.pes_data.geom
         self.poten      = self.pes_data.energies
         if 'deriv' in self.pes_data.data_keys:
-            self.deriv  = self.pes_data.grads
+            self.deriv  = self.pes_data.grads[self.pstates[1]]
         
     #-----------------------------------------------------------------------
     #
@@ -158,7 +182,7 @@ class Centroid:
         if np.linalg.norm(self.pes_geom - self.x()) > glbl.fpzero:
             print('WARNING: trajectory.derivative() called, ' +
                   'but pes_geom != trajectory.x(). ID=' + str(self.tid))
-        return self.deriv[self.pstates[1]]
+        return self.deriv
 
     #------------------------------------------------------------------------
     #

src/basis/trajectory.py
@@ -33,7 +33,7 @@ def copy_traj(orig_traj):
     new_traj.pes_geom   = copy.deepcopy(orig_traj.pes_geom)
     new_traj.poten      = copy.deepcopy(orig_traj.poten)
     new_traj.deriv      = copy.deepcopy(orig_traj.deriv)
-    new_traj.pes_data   = orig_traj.interface.copy_data(orig_traj.pes_data)
+    new_traj.pes_data   = orig_traj.interface.copy_surface(orig_traj.pes_data)
     return new_traj
 
 
@@ -142,8 +142,8 @@ class Trajectory:
         if glbl.fms['phase_prop'] != 0:
             self.gamma = phase
 #        self.gamma = 0.5 * np.dot(self.x(), self.p())
-        if abs(self.gamma) > 2*np.pi:
-            self.gamma = self.gamma - int(self.gamma/(2. * np.pi)) * 2. * np.pi
+            if abs(self.gamma) > 2*np.pi:
+                self.gamma = self.gamma - int(self.gamma/(2.*np.pi))*2.*np.pi
 
     def update_amplitude(self, amplitude):
         """Updates the amplitude of the trajectory."""
@@ -151,7 +151,7 @@ class Trajectory:
 
     def update_pes(self, pes_info):
         """Updates information about the potential energy surface."""
-        self.pes_data   = self.interface.copy_data(pes_info)
+        self.pes_data   = self.interface.copy_surface(pes_info)
         self.pes_geom   = self.pes_data.geom
         self.poten      = self.pes_data.energies
         if 'deriv' in self.pes_data.data_keys:
@@ -279,7 +279,7 @@ class Trajectory:
         else:
             return (self.kinetic() - self.potential() -
                     sum(2. * self.widths() * self.interface.kecoeff))
-#            return 0.5*(-np.dot(self.force(),self.x())+np.dot(self.p(),self.p()))
+#            return 0.5*(np.dot(self.force(),self.x())+np.dot(self.p(),self.p()))
 
     def coupling_norm(self, c_state):
         """Returns the norm of the coupling vector."""

src/integrals/saddle_point.py
@@ -38,6 +38,7 @@ def s_integral(t1, t2, nuc_only=False, Snuc=None):
         if Snuc is None:
             return nuclear.overlap(t1.phase(),t1.widths(),t1.x(),t1.p(),
                                    t2.phase(),t2.widths(),t2.x(),t2.p())
+#            return nuclear.overlap(t1,t2) 
         else:
             return Snuc
 
@@ -56,6 +57,7 @@ def v_integral(t1, t2, centroid=None, Snuc=None):
     if Snuc is None:
         Snuc = nuclear.overlap(t1.phase(),t1.widths(),t1.x(),t1.p(),
                                t2.phase(),t2.widths(),t2.x(),t2.p())
+#        Snuc = nuclear.overlap(t1,t2)
 
     # off-diagonal matrix element, between trajectories on the same
     # state (this also requires the centroid be present)
@@ -72,9 +74,10 @@ def v_integral(t1, t2, centroid=None, Snuc=None):
     elif t1.state != t2.state:
         # Derivative coupling
         fij = centroid.derivative()
-        v = np.vdot(fij, 2.* interface.kecoeff *
-                    nuclear.deldx(Snuc,t1.phase(),t1.widths(),t1.x(),t1.p(),
-                                       t2.phase(),t2.widths(),t2.x(),t2.p()))
+        v = 2.*np.vdot(fij, interface.kecoeff *
+                       nuclear.deldx(Snuc,t1.phase(),t1.widths(),t1.x(),t1.p(),
+                                          t2.phase(),t2.widths(),t2.x(),t2.p()))
+#                       nuclear.deldx(t1, t2, S=Snuc))
         # Scalar coupling
         if glbl.fms['coupling_order'] > 1:
             v += centroid.scalar_coup() * Snuc
@@ -93,10 +96,12 @@ def ke_integral(t1, t2, Snuc=None):
     else:
         if Snuc is None:
             Snuc = nuclear.overlap(t1.phase(),t1.widths(),t1.x(),t1.p(),
-                                   t2.phase(),t2.widths(),t2.x(),t2.p())
+                                  t2.phase(),t2.widths(),t2.x(),t2.p())
+#            Snuc = nuclear.overlap(t1,t2)
 
         ke = nuclear.deld2x(Snuc,t1.phase(),t1.widths(),t1.x(),t1.p(),
                                  t2.phase(),t2.widths(),t2.x(),t2.p())
+#        ke = nuclear.deld2x(t1, t2, S=Snuc)
         return -sum( ke * interface.kecoeff)
 
 # time derivative of the overlap
@@ -109,12 +114,15 @@ def sdot_integral(t1, t2, Snuc=None):
         if Snuc is None:
             Snuc = nuclear.overlap(t1.phase(),t1.widths(),t1.x(),t1.p(),
                                    t2.phase(),t2.widths(),t2.x(),t2.p())
-
+#            Snuc = nuclear.overlap(t1, t2, S=Snuc)
+    
         deldx = nuclear.deldx(Snuc,t1.phase(),t1.widths(),t1.x(),t1.p(),
                                    t2.phase(),t2.widths(),t2.x(),t2.p())
         deldp = nuclear.deldp(Snuc,t1.phase(),t1.widths(),t1.x(),t1.p(),
                                    t2.phase(),t2.widths(),t2.x(),t2.p())
-
-        sdot = (-np.dot(t2.velocity(),deldx) + np.dot(t2.force(), deldp) 
+#        deldx = nuclear.deldx(t1, t2, S=Snuc)
+#        deldp = nuclear.deldp(t1, t2, S=Snuc)
+ 
+        sdot = (np.dot(deldx,t2.velocity()) + np.dot(deldp,t2.force()) 
                 +1j * t2.phase_dot() * Snuc)
         return sdot

src/interfaces/vibronic.py
@@ -55,7 +55,7 @@ class Surface:
         self.adiabat_deriv = np.zeros((t_dim, n_states, n_states))
 
 
-def copy_data(orig_info):
+def copy_surface(orig_info):
     """Creates a copy of a Surface object."""
     
     # Perhaps should have more robust checking that "orig_info" is in fact