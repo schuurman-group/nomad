fixed sign error in integrals.deldx

src/integrals/crude_adiabatic_vibronic.py
@@ -5,7 +5,7 @@ potentials
 This currently uses first-order saddle point.
 """
 import numpy as np
-import src.integrals.nuclear_gaussian as nuclear
+import src.integrals.nuclear_cs_gaussian as nuclear
 import src.interfaces.vibronic as vibronic 
 import src.interfaces.vcham.hampar as ham
 
@@ -82,7 +82,7 @@ def prim_v_integral(N, a1, x1, p1, a2, x2, p2, gauss_overlap=None):
     # generally these should be 1D harmonic oscillators. If
     # multi-dimensional, the final result is a direct product of
     # each dimension
-    v_int = 0j
+    v_int = complex(0.,0.)
     for i in range(n_2):
         v_int += (a**(i-N) * b**(N-2*i) /
                  (np.math.factorial(i) * np.math.factorial(N-2*i)))
@@ -123,7 +123,7 @@ def v_integral(traj1, traj2, centroid=None, Snuc=None):
     # Fill in the upper-triangle
     for s1 in range(nst-1):
         for s2 in range(s1+1, nst):
-            h_nuc[s1,s2] = 0j
+            h_nuc[s1,s2] = complex(0.,0.)
             h_nuc[s2,s1] = h_nuc[s1,s2]
 
 #    print("v integral: "+str(h_nuc))
@@ -149,7 +149,7 @@ def ke_integral(traj1, traj2, centroid=None, Snuc=None):
 
     
 # time derivative of the overlap
-def sdot_integral(traj1, traj2, centroid=None, Snuc=None):
+def sdot_integral(traj1, traj2, centroid=None, Snuc=None, e_only=False, nuc_only=False):
     """Returns the matrix element <Psi_1 | d/dt | Psi_2>."""
     if Snuc is None:
         Snuc = nuclear.overlap(traj1.phase(),traj1.widths(),traj1.x(),traj1.p(),
@@ -166,37 +166,47 @@ def sdot_integral(traj1, traj2, centroid=None, Snuc=None):
                                traj2.phase(),traj2.widths(),traj2.x(),traj2.p())
 
     # the nuclear contribution to the sdot matrix
-    sdot = ( -np.dot(traj2.velocity(), deldx) + np.dot(traj2.force(), deldp) 
-            + 1j * traj2.phase_dot() * Snuc) * Selec
+#    sdot = ( np.dot(traj2.velocity(), deldx) + np.dot(traj2.force(), deldp) 
+#            + 1j * traj2.phase_dot() * Snuc) * Selec
+    sdot = (  np.dot(deldx, traj2.velocity()) + np.dot(deldp, traj2.force()) ) * Selec
+    print('vel, for='+str(np.dot(deldx, traj2.velocity()))+' '+str(np.dot(deldp, traj2.force())))
+
+    if nuc_only:
+        return sdot
 
     # the derivative coupling
-#    dia      = traj2.pes_data.diabat_pot
-#    diaderiv = traj2.pes_data.diabat_deriv
-#    v12      = dia[0,1]
-#    de       = dia[1,1] - dia[0,0]
-#    argt     = 2. * v12 / de
-#    t1       = 0.5 * np.arctan2(2.*v12, de)
+    dia      = traj2.pes_data.diabat_pot
+    diaderiv = traj2.pes_data.diabat_deriv
+    v12      = dia[0,1]
+    de       = dia[1,1] - dia[0,0]
+    argt     = 2. * v12 / de
+    t1       = 0.5 * np.arctan2(2.*v12, de)
 
     # ensure theta agrees with dat matrix
-#    pi_mult  = [-2.,1.,0.,1.,2.]
-#    dif_vec  = np.array([np.linalg.norm(traj2.pes_data.dat_mat - 
-#                                 np.array([[np.cos(t1+i*np.pi),-np.sin(t1+i*np.pi)],
-#                                           [np.sin(t1+i*np.pi),np.cos(t1+i*np.pi)]])) 
-#                                           for i in pi_mult])
+    pi_mult  = [-2.,1.,0.,1.,2.]
+    dif_vec  = np.array([np.linalg.norm(traj2.pes_data.dat_mat - 
+                                 np.array([[np.cos(t1+i*np.pi),-np.sin(t1+i*np.pi)],
+                                           [np.sin(t1+i*np.pi),np.cos(t1+i*np.pi)]])) 
+                                           for i in pi_mult])
 
-#    theta = t1 + pi_mult[np.argmin(dif_vec)]*np.pi
-#    dtheta = np.array([(diaderiv[q,0,1]/de - 
-#                        v12*(diaderiv[q,1,1]- diaderiv[q,0,0])/de**2)/(1+argt**2) 
-#                        for q in range(traj2.dim)])
+    theta = t1 + pi_mult[np.argmin(dif_vec)]*np.pi
+    dangle = np.array([(diaderiv[q,0,1]/de - 
+                        v12*(diaderiv[q,1,1]- diaderiv[q,0,0])/de**2)/(1+argt**2) 
+                        for q in range(traj2.dim)])
     
 
-#    dphi  = np.array([[-np.sin(theta),-np.cos(theta)],
-#                      [ np.cos(theta),-np.sin(theta)]])
-#    deriv_coup = np.array([np.dot(traj1.pes_data.dat_mat[:,traj1.state],
-#                                  dphi[:,traj2.state]*dtheta[q]) for q in range(traj2.dim)])
+    dphi  = np.array([[-np.sin(theta),-np.cos(theta)],
+                      [ np.cos(theta),-np.sin(theta)]])
+    dtheta  = np.array([[dphi[i,traj2.state]*dangle[q] for q in range(traj2.dim)] for i in range(traj2.nstates)])
+    deriv_coup = np.array([np.dot(traj1.pes_data.dat_mat[:,traj1.state],dtheta[:,q]) for q in range(traj2.dim)])
+
+    if e_only:
+        print("deriv_coup, t2.vel="+str(deriv_coup)+' '+str(traj2.velocity()))
+        print("phi1, dphi2="+str(traj1.pes_data.dat_mat[:,traj1.state])+' '+str(dtheta))
+        return np.dot(deriv_coup, traj2.velocity()) * Snuc
 
     # time-derivative of the electronic component
-#    sdot += np.dot(deriv_coup, traj2.velocity()) * Snuc
+    sdot += np.dot(deriv_coup, traj2.velocity()) * Snuc
 
     return sdot
  