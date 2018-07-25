bug fix for bra_ket averaged integral routine

nomad/integrals/bra_ket_averaged.py
@@ -5,6 +5,7 @@ traveling on adiabataic potentials
 import numpy as np
 import nomad.core.glbl as glbl
 import nomad.compiled.nuclear_gaussian as nuclear
+import nomad.compiled.vibronic_gaussian as vibronic
 
 # Let propagator know if we need data at centroids to propagate
 require_centroids = False
@@ -38,19 +39,20 @@ def v_integral(t1, t2, nuc_ovrlp=None):
         vji = t2.energy(state) * Sji
 
         if glbl.propagate['integral_order'] > 0:
-            o1_ij = nuclear.ordr1_vec(t1.widths(),t1.x(),t1.p(),
-                                      t2.widths(),t2.x(),t2.p())
-            o1_ji = nuclear.ordr1_vec(t2.widths(),t2.x(),t2.p(),
-                                      t1.widths(),t1.x(),t1.p())
+
+            o1_ij = vibronic.qn_vector(1,t1.widths(),t1.x(),t1.p(),
+                                         t2.widths(),t2.x(),t2.p())
+            o1_ji = vibronic.qn_vector(1,t2.widths(),t2.x(),t2.p(),
+                                         t1.widths(),t1.x(),t1.p())
             vij += np.dot(o1_ij - t1.x()*Sij, t1.derivative(state,state))
             vji += np.dot(o1_ji - t2.x()*Sji, t2.derivative(state,state))
 
         if glbl.propagate['integral_order'] > 1:
             xcen  = (t1.widths()*t1.x() + t2.widths()*t2.x()) / (t1.widths()+t2.widths())
-            o2_ij = nuclear.ordr2_vec(t1.widths(),t1.x(),t1.p(),
-                                      t2.widths(),t2.x(),t2.p())
-            o2_ji = nuclear.ordr2_vec(t2.widths(),t2.x(),t2.p(),
-                                      t1.widths(),t1.x(),t1.p())
+            o2_ij = vibronic.qn_vector(2, t1.widths(),t1.x(),t1.p(),
+                                          t2.widths(),t2.x(),t2.p())
+            o2_ji = vibronic.qn_vector(2, t2.widths(),t2.x(),t2.p(),
+                                          t1.widths(),t1.x(),t1.p())
 
             for k in range(t1.dim):
                 vij += 0.5*o2_ij[k]*t1.hessian(state)[k,k]
@@ -72,10 +74,11 @@ def v_integral(t1, t2, nuc_ovrlp=None):
     else:
         # Derivative coupling
         fij = t1.derivative(t1.state, t2.state)
-        vij = 2.*np.vdot(t1.derivative(t1.state,t2.state), t1.kecoef *
+        fji = t2.derivative(t2.state, t1.state)
+        vij = 2.*np.vdot(fij, t1.kecoef *
                          nuclear.deldx(Sij,t1.widths(),t1.x(),t1.p(),
                                            t2.widths(),t2.x(),t2.p()))
-        vji = 2.*np.vdot(t2.derivative(t2.state,t1.state), t2.kecoef *
+        vji = 2.*np.vdot(fji, t2.kecoef *
                          nuclear.deldx(Sji,t2.widths(),t2.x(),t2.p(),
                                            t1.widths(),t1.x(),t1.p()))
     return 0.5*(vij + vji.conjugate())