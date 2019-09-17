f9a89b41d5c89f5ccf59d14856e2a88fa8e3916e
Michael Schuurman <mschuurm@gmail.com>
2019-09-17 10:48:59 -0400

    generalized velocity-verlet so any quantity can be propagated -- not just traj/wfn

--- nomad/propagators/velocity_verlet.py
+++ nomad/propagators/velocity_verlet.py
@@ -10,9 +10,19 @@ import nomad.core.glbl as glbl
 import nomad.core.timings as timings
 import nomad.core.surface as evaluate
 
-
 propphase = glbl.properties['phase_prop']
 
+@timings.timed
+def propagate(q0, cf, dq, dt):
+    """the fundamental routine for propagating a quantity using velocity-verlet
+       algorithm"""
+
+    # we're going to limit this to 2-nd order DE for time being...
+    q1 = q0
+    for i in range(len(dq)):
+        q1 += cf[i]*dq[i] * (dt**i)
+
+    return q1
 
 @timings.timed
 def propagate_wfn(wfn, dt):
@@ -20,7 +30,7 @@ def propagate_wfn(wfn, dt):
     # update position
     for i in range(wfn.n_traj()):
         if wfn.traj[i].active:
-            propagate_position(wfn.traj[i], dt)
+            propagate_half1(wfn.traj[i], dt)
 
     # update electronic structure for all trajectories
     # and centroids (where necessary)
@@ -29,28 +39,29 @@ def propagate_wfn(wfn, dt):
     # finish update of momentum and phase
     for i in range(wfn.n_traj()):
         if wfn.traj[i].active:
-            propagate_momentum(wfn.traj[i], dt)
-
+            propagate_half2(wfn.traj[i], dt)
 
 @timings.timed
 def propagate_trajectory(traj, dt):
     """Propagates a single trajectory with VV."""
+
     # position update
-    propagate_position(traj, dt)
+    propagate_half1(traj, dt)
 
     # update electronic structure
     evaluate.update_pes_traj(traj)
 
     # momentum/phase update
-    propagate_momentum(traj, dt)
+    propagate_half2(traj, dt)
 
 
-def propagate_position(traj, dt):
+def propagate_half1(traj, dt):
     """Updates the position to end of time step and half-propagate the
     momentum and phase."""
     # set the t=t values
     x0 = traj.x()
     p0 = traj.p()
+    g0 = traj.phase()
     v0 = traj.velocity()
     f0 = traj.force()
     m  = traj.masses()
@@ -61,49 +72,38 @@ def propagate_position(traj, dt):
     # potential energy]
     if propphase:
         # half update phase
-        #  gamma = gamma + dt * phase_dot / 2.0
-        g1_0 = traj.phase_dot()
-        g2_0 = 2. * np.dot(f0, v0)
-        dgamma = g1_0 * dt / 2. - g2_0 * dt**2 / 8.
-        traj.update_phase(traj.phase() + dgamma)
+        g_dot = traj.phase_dot()
+        g_cur = np.dot(f0, v0)
+        g1    = propagate(g0, [0, 0.5, -0.25], [0, g_dot, g_cur], dt)
+        traj.update_phase(g1)
 
     # update position and momentum
     #   x(t+dt) = x(t) + v(t)*dt + 0.5*a(t)*dt^2
     #   p(t+dt) = p(t) + 0.5*m*(a(t) + a(t+dt))*dt
     # --> need to compute forces at new geometry
-    x1 = x0 + v0*dt + 0.5 * (f0 / m) * dt**2
+    x1 = propagate(x0, m, [0, 1., 0.5], [0, v0, f0/m], dt)
+    p1 = propagate(p0, m, [0, 0.5], [0, f0], dt)
 
-    # update x
+    # update x, and half-update p
     traj.update_x(x1)
+    traj.update_p(p1)
 
-    # half update p and phase
-    traj.update_p(p0 + 0.5 * f0 * dt)
-
-
-def propagate_momentum(traj, dt):
+def propagate_half2(traj, dt):
     """Finish the phase and momentum update using forces and velocities at
     the final position."""
     # half update p
     f1 = traj.force()
-    p1 = traj.p() + 0.5 * f1 * dt
-    traj.update_p(p1)
+    p1 = traj.p()
     v1 = traj.velocity()
+    g1 = traj.phase()
 
     if propphase:
         # update the nuclear phase
-        g1_1 = traj.phase_dot()
-        g2_1 = 2. * np.dot(f1, v1)
+        g_dot = traj.phase_dot()
+        g_cur = np.dot(f1, v1)
+        #delta_gamma = (gdot1 + gdot2) * dt / 2.0 - (gcur1 - gcur2) * dt**2 / 4.
+        g2    = propagate(g1, [0, 0.5, 0.25], [0, g_dot, g_cur], dt)
+        traj.update_phase(g2)
 
-        # solve for the phases
-        #a = 0.5 * dt**2
-        #b = (1./6.) * dt**3
-        #c = dt
-        #d = 0.5 * dt**2
-
-        #vec   = np.array([g1_1 - g1_0 - g2_0 * dt, g2_1 - g2_1])
-        #alpha =( d*vec[0] - b*vec[1]) / (a*d - b*c)
-        #beta  =(-c*vec[0] - a*vec[1]) / (a*d - b*c)
-
-        #dgamma = (g1_0 + g1_1) * dt / 2.0 - (g2_0 - g2_1) * dt**2 / 8.
-        dgamma = g1_1 * dt / 2. + g2_1 * dt**2 / 8.
-        traj.update_phase(traj.phase() + dgamma)
+    p2 = propagate(p1, [0, 0.5], [0, f1], dt)
+    traj.update_p(p2)
