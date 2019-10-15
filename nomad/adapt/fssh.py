0d87b3debb3627967ef5ab24fba99115377ac3df
Ashley Elgersma <ashley.elgersma@lab.kingsu.ca>
2019-10-15 17:11:12 -0400

    Changed variable propagated from state populations to state amplitudes

--- nomad/adapt/fssh.py
+++ nomad/adapt/fssh.py
@@ -14,22 +14,22 @@ import numpy as np
 import scipy as sp 
 import nomad.core.glbl as glbl
 import nomad.core.log as log
-import scipy.constants as sp_con
-#random.seed(4611)
 
-a_cache = {}
+random.seed(464)
+
+c_cache = {}
 data_cache = {}
+n_states = glbl.properties['n_states']
 #initial state probability (a) matrix:
-def init_a():
+def init_c():
     init_state = glbl.properties['init_state']
-    n_states = glbl.properties['n_states']
-    init_a_matrix = np.zeros((n_states, n_states), dtype = 'complex')
-    init_a_matrix[init_state,init_state] = 1
-    a_cache['init'] = init_a_matrix
+    init_c = np.zeros((n_states), dtype = 'complex')
+    init_c[init_state] = 1
+    c_cache['init'] = init_c
 
-init_a()
-
-current_a = a_cache['init']
+init_c()
+switch_times = 0
+current_c = c_cache['init']
 
 def adapt(wfn, dt):
     """Calculates the probability of a switch between states, tests this against a random number, and switches states accordingly."""
@@ -41,36 +41,48 @@ def adapt(wfn, dt):
     #the "local time", or time between larger time steps:
     local_time = current_time
     #the smaller time step:
-    local_dt = dt/10
+    local_dt = dt/100
     current_st = traj.state
-
-        #Check that we only have one trajectory:
+        
+    #Check that we only have one trajectory:
     if wfn.n_traj() > 1:
         sys.exit('fssh algorithm must only have one trajectory')
 
 
-    def propagate_a():
-        """propagates the matrix of state probabilities and coherences"""
+    def propagate_c():
+        """propagates the values of c"""
         
-        a_copy = current_a.copy()
-
+        c_copy = current_c.copy()
         n_states = glbl.properties['n_states']
         for k in range(n_states):
-            for  j in range(n_states):
-                def a_dot(a):
-                    """returns the time derivative of the specified a matrix element"""
-                    dq0 = 0
-                    dq1 = 0
-                    for l in range(n_states):
-                        term1 = a_copy[l,j] * np.complex(diabat_pot(k,l), -nac(k,l))
-                        term2 = a_copy[k,l] * np.complex(diabat_pot(l,j), -nac(l,j))
-                        both_terms = (term1 - term2) / (np.complex(0,1))
-                        dq1 += both_terms
-                    dq = [dq1, 0, 0, 0]
-                    return dq
-                #actually propagate a
-                akj =  glbl.modules['propagator'].propagate([a_copy[k,j]], a_dot, local_dt)
-                current_a[k,j] = akj
+            def this_c_dot(ck):
+                return [c_dot(c_copy, ck, k)]
+            #actually propagate c
+            ck =  glbl.modules['propagator'].propagate([c_copy[k]], this_c_dot, local_dt)
+            current_c[k] = ck
+
+
+    def c_dot(c, ck, k):
+        """returns the first time derivative of ci"""
+        c_label = 'c_dot' +str(k) + str(local_time)
+        if c_label in c_cache:
+            return c_cache[c_label]
+        dq1 = 0
+        for j in range(n_states):
+            if j == k:
+                dq1 += ck[0] * np.complex(diabat_pot(k,j), -nac(k,j))
+            else:
+                dq1 += c[j] * np.complex(diabat_pot(k,j), -nac(k,j))     
+            
+            c_dot= np.complex(0, -dq1)
+        c_cache[c_label] = c_dot 
+        
+        return c_dot
+    
+
+
+
+
 
     def diabat_pot(j,k):
         """Returns the specified matrix element of the diabatic potential"""
@@ -87,34 +99,73 @@ def adapt(wfn, dt):
         data_cache[nac_label] = coup
         return coup
 
+    def a(k,j):
+        '''returns a = c c*'''
+        return current_c[k] * np.conj(current_c[j])
 
-
+    
     #this loop is called for each small time step 
     while local_time < current_time + dt:
+        current_st = traj.state
+        
+
+
         random_number = random.random() 
         prev_prob = 0
         for st in range(glbl.properties['n_states']):
         #can only switch between states
             if st == current_st:
                 continue
-            #Calculate switching probability for this transition:
-            state_pop = current_a[current_st, current_st]
-            state_flux = (2 * np.imag(np.conj(current_a[current_st, st]) * diabat_pot(current_st, st))) - (2 * np.real(np.conj(current_a[current_st, st]) * nac(current_st, st)))
-                
-            switch_prob = state_flux * local_dt / np.real(state_pop)
+            state_pop = a(current_st,current_st) 
+            state_flux = (2*np.imag(np.conj(a(current_st, st)) * diabat_pot(current_st, st))) - ( 2* np.real(np.conj(a(current_st, st)) * nac(current_st, st)))
+            switch_prob = state_flux * local_dt / state_pop
             if switch_prob < 0:     
                 switch_prob = 0
 
             #add to the previous probability
             switch_prob = prev_prob + switch_prob
-              
-        
-            switch_times = 0
+
+
+            #a test:
+            def a_dot(k,j):
+                adq = 0
+                for l in range(n_states):
+                    term1 = a(l,j) * np.complex(diabat_pot(k,l), -nac(k,l))
+                    term2 = a(k,l) * np.complex(diabat_pot(l,j), -nac(l,j))
+                    both_terms = np.complex(0, (term2-term1))
+                    adq+= both_terms
+                return adq
+            #-----------------------------------------------------------------------------
+            #plt.plot(traj.x()[0], switch_prob, marker='o', markerSize = 3, color = 'brown')              
+            #plt.plot(traj.x()[0], current_st, marker='o', markerSize = 3, color = 'blue')              
+            #plt.plot(traj.x()[0], diabat_pot(0,1), marker='o', markerSize = 3, color = 'red') 
+            #plt.plot(traj.x()[0], diabat_pot(1,0), marker='o', markerSize = 3, color = 'orange') 
+            #plt.plot(traj.x()[0], diabat_pot(0,0), marker='o', markerSize = 3, color = 'brown') 
+            #plt.plot(traj.x()[0], diabat_pot(1,1), marker='o', markerSize = 3, color = 'green') 
+            #plt.plot(local_time,a_dot(0,0, [current_a[0,0]]), marker='x', markerSize = 3, color = 'red') 
+            #plt.plot(local_time,np.imag(a_dot(0,0, [current_a[0,0]])), marker='x', markerSize = 3, color = 'green') 
+            #plt.plot(local_time,-state_flux + a_dot(0,0, [current_a[0,0]]), marker='x', markerSize = 3, color = 'purple') 
+            #plt.plot(traj.x()[0], a_dot(0,1, [current_a[0,1]]), marker='x', markerSize = 3, color = 'orange')
+            #plt.plot(local_time, a_dot(1,1), marker='x', markerSize = 3, color = 'blue')
+            #plt.plot(local_time, a_dot(0,0), marker='x', markerSize = 3, color = 'green')
+            #plt.plot(local_time, nac(0,1), marker='x', markerSize = 3, color = 'red') 
+            #plt.plot(local_time, nac(1,0), marker='x', markerSize = 3, color = 'orange') 
+            #plt.plot(traj.x()[0], nac(0,0), marker='x', markerSize = 3, color = 'brown') 
+            #plt.plot(traj.x()[0], nac(1,1), marker='x', markerSize = 3, color = 'green') 
+            #plt.plot(local_time, state_pop, marker='o', markerSize = 3, color = 'green')
+            #plt.plot(local_time, state_flux, marker='x', markerSize = 3, color = 'black')
+            #plt.plot(traj.x()[0], a(0,0), marker='x', markerSize = 3, color = 'green')
+            #plt.plot(traj.x()[0], a(1,1), marker='x', markerSize = 3, color = 'blue')
+            #plt.plot(traj.x()[0], a(0,0) + a(1,1), marker='x', markerSize = 3, color = 'pink')
+            #plt.plot(traj.x()[0], 1, marker='o', markerSize = 3, color = 'pink')
+            ##-----------------------------------------------------------------------------------
+
+
             #Check probability against random number, see if there's a switch
             if prev_prob < random_number <= switch_prob: 
                 total_E = traj.classical()
                 current_T = traj.kinetic() 
-                log.print_message('general',['Attempting surface hop to state ' + str(st)]) 
+                log.print_message('general',['Attempting surface hop to state ' + str(st) + ' at time ' + str(local_time)]) 
                 #change the state:
                 traj.state = st
                 new_V = traj.potential()
@@ -124,33 +175,36 @@ def adapt(wfn, dt):
                     log.print_message('general',['Surface hop failed (not enough T), remaining on state ' + str(current_st)])
                     traj.state = current_st
                 else:
-                    log.print_message('general',['Surface hop successful, switching to state ' + str(st)])
+                    log.print_message('general',['Surface hop successful, switching from state ' + str(current_st) + ' to state ' + str(st)])
                     #calculate and set the new momentum:
                     scale_factor = np.sqrt(new_T/current_T)
                     current_p = traj.p()
                     new_p = scale_factor * current_p
                     traj.update_p(new_p)
                     #to keep track, for now:
+                    global switch_times
                     switch_times += 1
-        if local_time >= glbl.properties['simulation_time'] +glbl.properties['default_time_step'] - (1.5 * local_dt):
+
+                continue      
+
+        if local_time >= glbl.properties['simulation_time'] +glbl.properties['default_time_step'] - local_dt:
+            #plt.show()
             if traj.x()[0]>0:
                 if switch_times <= 1:
                     print(current_st)
                 else:
-                    print(switch_times)
-
+                    if current_st == 0:
+                        print(2)
+                    if current_st ==1:
+                        print(3)
             else:
-                if switch_times <= 1:
-                    print(current_st + 5)
-                else:
-                    print(switch_times + 5)
+                print(4)
 
-
-        propagate_a()
+        propagate_c()
         #update the local time:
         local_time = local_time + local_dt
 
 
 def in_coupled_regime(wfn):
     return False
-   
+
